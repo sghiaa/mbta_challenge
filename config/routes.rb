f4f2d919dba2813f15763d67fe2aad75d02438b3
sghiaa <asghiah@gmail.com>
2016-07-29 10:57:45 -0400

    make root route the departures page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'departures#show'
   get 'departures' => 'departures#show'
 
   # Example of regular route:
